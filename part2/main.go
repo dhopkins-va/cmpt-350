cc7c4372a9dcdd48b4cc859b7909425bc5103a2c
Dale Hopkins <dhopkins@vendasta.com>
2018-03-21 12:43:46 -0600

    Add part3

--- part2/main.go
+++ part2/main.go
@@ -9,13 +9,14 @@ import (
 
 type TimeResponse struct {
 	Time time.Time `json:"time"`
+	Server string `json:"server"`
 }
 
 func main() {
 	fmt.Printf("Server Started\n")
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(TimeResponse{Time: time.Now()})
+		json.NewEncoder(w).Encode(TimeResponse{Time: time.Now(), Server:"Container"})
 		fmt.Printf("* Request Handled")
 	})
 	http.ListenAndServe(":80", nil)
